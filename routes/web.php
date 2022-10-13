8fc25426d05b0339ec2d6c1932765f772b116dde
Thainá Pires <thainaspiress@gmail.com>
2022-10-12 22:54:21 -0300

    feat: adding views sobre-nos and contato

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Olá, seja bem-vindo ao curso!';
-});
+Route::get('/', 'PrincipalController@principal');
 
-Route::get('/sobre-nos', function () {
-    return 'Sobre nós';
-});
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
-Route::get('/contato', function () {
-    return 'Contato';
-});
+Route::get('/contato', 'ContatoController@contato');
 
 //Route::get($uri, $callback)

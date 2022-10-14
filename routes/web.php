d521df32b8638ca78433659328f3f128fbe7f587
Thainá Pires <thainaspiress@gmail.com>
2022-10-14 00:01:44 -0300

    feat: adding more routes and grouping some

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,24 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get(
+Route::get('/login', function(){
+    return 'Login';
+});
+
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){
+        return 'Clientes';
+    });
+    Route::get('/fornecedores', function(){
+        return 'Fornecedores';
+    });
+    Route::get('/produtos', function(){
+        return 'Produtos';
+    });
+});
+
+/* Route::get(
     '/contato/{nome}/{categoria_id?}', 
     function(
         string $nome, 
@@ -28,5 +45,5 @@ Route::get(
         echo "$nome - $categoria_id";
     }
 )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
-
+ */
 //Route::get($uri, $callback)

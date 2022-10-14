5feb3cd5a655e521955cc45fde517a274ced9adf
Thainá Pires <thainaspiress@gmail.com>
2022-10-14 00:17:03 -0300

    feat: naming routes, redirecting and adding route fallback

--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
-
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
-Route::get('/contato', 'ContatoController@contato');
-
-Route::get('/login', function(){
-    return 'Login';
-});
-
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){ return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){
-        return 'Clientes';
-    });
-    Route::get('/fornecedores', function(){
-        return 'Fornecedores';
-    });
-    Route::get('/produtos', function(){
-        return 'Produtos';
-    });
+    Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
+});
+
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+//Route::redirect('/rota2', '/rota1'); //Usando redirect do route
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
 });
 
 /* Route::get(

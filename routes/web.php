24cd184d9241884cf96235e57120016a5f9dbafe
Thainá Pires <thainaspiress@gmail.com>
2022-10-13 23:51:26 -0300

    feat: adding new routes to learn parameters settings

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,14 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
+Route::get(
+    '/contato/{nome}/{categoria_id?}', 
+    function(
+        string $nome, 
+        int $categoria_id = 1 // 1 - Informação
+    ){
+        echo "$nome - $categoria_id";
+    }
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+
 //Route::get($uri, $callback)

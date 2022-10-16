8445ec96676cd6604ca979b14122772e47d52a34
Thainá Pires <thainaspiress@gmail.com>
2022-10-15 22:21:06 -0300

    feat: how to use if in blades

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -9,3 +9,13 @@
     // Comentando utilizando sintaxe nativa php
     echo 'Teste';
 @endphp
+
+{{--@dd($fornecedores)--}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif

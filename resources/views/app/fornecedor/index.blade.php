501716d4dac488d1e1a143ceb3c4409e44ab239f
Thainá Pires <thainaspiress@gmail.com>
2022-10-15 22:12:35 -0300

    feat: adding comments and php pure code in blades

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,11 @@
+<h3>Fornecedor</h3>
+
+{{ 'Teste' }}
+<?= 'Teste' ?>
+
+{{--Fica o comentário que será descartado pelo interpretador do blade--}}
+
+@php
+    // Comentando utilizando sintaxe nativa php
+    echo 'Teste';
+@endphp

b5c27e0635703c52e48f6d2068ca18b248b3847b
Thainá Pires <thainaspiress@gmail.com>
2022-11-11 11:21:38 -0300

    feat: adding seeders

---
+++ database/seeds/FornecedorSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Fornecedor;
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Instanciando o objeto
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'contato@fornecedor100.com.br';
+        $fornecedor->save();
+
+        //Usando o metodo create, atenção para o atributo fillable da classe
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'RS',
+            'email' => 'contato@fornecedor200.com.br'
+        ]);
+
+        //Através do insert. Não popula as colunas created_at, updated_at
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'fornecedor300.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor300.com.br'
+        ]);
+    }
+}
---
+++ database/seeds/SiteContatoSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\SiteContato;
+
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*
+        $contato = new SiteContato();
+        $contato->nome = 'Sistema SG';
+        $contato->telefone = '(21) 99999-9999';
+        $contato->email = 'contato@sg.com.br';
+        $contato->motivo_contato = 1;
+        $contato->mensagem = 'Bem vindo ao sistema Super gestão';
+        $contato->save();
+        */
+
+        factory(SiteContato::class, 100)->create();
+    }
+}

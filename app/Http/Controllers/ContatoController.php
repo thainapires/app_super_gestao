6a24e6a455f83e24e20e088d2dc493e8de9b4d1e
Thainá Pires <thainaspiress@gmail.com>
2022-11-16 16:48:19 -0300

    feat: add basic validation to form fields

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -27,9 +27,27 @@ class ContatoController extends Controller
         print_r($contato->getAttributes());
         $contato->save();*/
 
-        $contato = new SiteContato();
-        $contato->create($request->all());
+        /*$contato = new SiteContato();
+        $contato->create($request->all());*/
 
-        return view('site.contato', ['titulo' => 'Contato']);
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
+    }
+
+    public function salvar(Request $request){
+        //Validação dos dados
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000',
+        ]);
+        //SiteContato::create($request->all());
     }
 }

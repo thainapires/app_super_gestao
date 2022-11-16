c637be66ff45f1a9b8fd97f3aa995d966f7677a0
Thainá Pires <thainaspiress@gmail.com>
2022-11-16 15:27:32 -0300

    feat: retrieve form data and store in database

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,10 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
+        //print_r($request->all());
+        //print_r($request->input('email'));
+
+        /*
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        $contato->save();
+        */
+
+        /*$contato = new SiteContato();
+        $contato->fill($request->all());
+        print_r($contato->getAttributes());
+        $contato->save();*/
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+
         return view('site.contato', ['titulo' => 'Contato']);
     }
 }

62d31eade4bb9313d06e80c4524bed65fe1c6f97
Thainá Pires <thainaspiress@gmail.com>
2022-12-03 18:44:48 -0300

    section-12: add part 1 of crud of fornecedores

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
     Route::get('/sair', 'LoginController@sair')->name('app.sair');
     Route::get('/cliente', 'ClienteController@index')->name('app.cliente');
     Route::get('/fornecedor','FornecedorController@index')->name('app.fornecedor');
+    Route::post('/fornecedor/listar','FornecedorController@listar')->name('app.fornecedor.listar');
+    Route::get('/fornecedor/adicionar','FornecedorController@adicionar')->name('app.fornecedor.adicionar');
+    Route::post('/fornecedor/adicionar','FornecedorController@adicionar')->name('app.fornecedor.adicionar');
+    Route::get('/fornecedor/editar/{id}', 'FornecedorController@editar')->name('app.fornecedor.editar');
     Route::get('/produto', 'ProdutoController@index')->name('app.produto');
 });
 
